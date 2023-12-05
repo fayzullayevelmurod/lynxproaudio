c88b76d31484a01954988308b4da9582b892afd3
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-12-05 20:53:29 +0500

    header update

--- js/app.js
+++ js/app.js
@@ -5,4 +5,19 @@ var swiper = new Swiper(".homeSwiper", {
     autoplay: {
         delay: 5000,
       },
-  });
+});
+
+
+
+let nav_search = document.querySelector('.nav_search');
+let nav_search_block = document.querySelector('.nav_search_block');
+let search_block_close = document.querySelector('.search_block_close');
+
+nav_search.addEventListener('click', () => {
+    nav_search_block.classList.toggle('active');
+});
+
+
+search_block_close.addEventListener('click', () => {
+    nav_search_block.classList.remove('active');
+});
