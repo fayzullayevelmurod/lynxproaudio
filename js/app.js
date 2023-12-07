bb8dec49dfaffbe3cfaf6bc833d5d55599d383e3
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-12-07 10:37:55 +0500

    main the end

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// home slider
 var swiper = new Swiper(".homeSwiper", {
     spaceBetween: 30,
     effect: "fade",
@@ -12,7 +13,7 @@ var swiper = new Swiper(".homeSwiper", {
 });
 
 
-
+// search
 let nav_search = document.querySelector('.nav_search');
 let nav_search_block = document.querySelector('.nav_search_block');
 let search_block_close = document.querySelector('.search_block_close');
@@ -24,4 +25,18 @@ nav_search.addEventListener('click', () => {
 
 search_block_close.addEventListener('click', () => {
     nav_search_block.classList.remove('active');
+});
+
+
+
+// burger
+let burger_open = document.querySelector('.burger_open');
+let media_menu = document.querySelector('.media_menu');
+
+burger_open.addEventListener('click', () => {
+    media_menu.classList.toggle('active');
+});
+
+media_menu.addEventListener('click', () => {
+    media_menu.classList.remove('active');
 });
